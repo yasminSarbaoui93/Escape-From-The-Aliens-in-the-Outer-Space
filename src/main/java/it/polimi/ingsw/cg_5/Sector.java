de4ac226addc5829cd98eb30b8511c0121c779f4
andrea.paulillo <andrea.paulillo@libero.it>
2015-05-14 23:23:45 +0200

    prova push

--- src/main/java/it/polimi/ingsw/cg_5/Sector.java
+++ src/main/java/it/polimi/ingsw/cg_5/Sector.java
@@ -2,16 +2,22 @@ package it.polimi.ingsw.cg_5;
 
 import java.util.ArrayList;
 
-public abstract class Sector {
-
-private ArrayList <Character> playerList;
+public  class Sector {
+//private ArrayList <String> borderList;
+//private ArrayList <Character> playerList;
 private final String sectorName;
+
+
+public String getSectorName() {
+	return sectorName;
+}
+
+
 //lettura da file nome sector 
-public  Sector(String sectorName,ArrayList<String> border){
+public Sector(String sectorName){
 
 	this.sectorName=sectorName;
 //leggere da  file per settori confinanti
-	
 		
 }
 }
