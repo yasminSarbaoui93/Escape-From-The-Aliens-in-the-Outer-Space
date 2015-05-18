8fee567364c87434c9b5860669ac2938e3c9b8a2
Yasmin <Yasmin@10.168.65.132>
2015-05-18 16:37:40 +0200

    prova branch

--- src/main/java/it/polimi/ingsw/cg_5/Map.java
+++ src/main/java/it/polimi/ingsw/cg_5/Map.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 //Creazione mappa tramite Hashmap
 public class Map {
 
+	//ciao
 	private HashMap <String, Sector> map = new HashMap <String,Sector>();
 	
 	public void addSector(String name, Sector sector){
