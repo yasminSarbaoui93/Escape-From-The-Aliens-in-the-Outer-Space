47167c7d9a84554f113e7c6c92169a42770eca3a
andrea.paulillo <andrea.paulillo@libero.it>
2015-05-19 17:09:50 +0200

    Aggiunti commenti classe map

--- src/main/java/it/polimi/ingsw/cg_5/Map.java
+++ src/main/java/it/polimi/ingsw/cg_5/Map.java
@@ -10,17 +10,21 @@ import java.util.Scanner;
 public class Map  {
 	protected HashMap <String, Sector> map = new HashMap <String,Sector>();	
 	
-	public void Generator() { //metodo che crea mappa
+	public void Generator() { 
+	//metodo che crea la mappa leggendo dal file specificato nella chiamata(lo si implementerà più avanti,
+	//per ora chiamiamo il file della mappa Galilei. 
+	//Il file è strutturato per righe, ogni riga rappresenta un tipo di settore che andrà generato
 	String nomeSettore;
 	String tipoSettore="SAFE";
 	FileReader fileSettori;
 	try {
 		fileSettori = new FileReader("./src/main/java/it/polimi/ingsw/cg_5/Mappa_Galilei.txt");
 		Scanner in =new Scanner(fileSettori);
-		while(in.hasNextLine()){
-			while (in.hasNext() ){ // se la stringa non contiene "/"
+		while(in.hasNextLine()){ //leggiamo fino alla fine del file
+			while (in.hasNext() ){ 
 				nomeSettore=in.next();
-				if(nomeSettore.contains("/")==false){
+				if(nomeSettore.contains("/")==false){ // se la stringa non contiene "/" chiamiamo il costruttore
+													  //in base al tipo letto dal file
 					if(tipoSettore.equals("SAFE")){								
 						SafeSector settore= new SafeSector(nomeSettore);
 						this.addSector(nomeSettore , settore); 
@@ -36,6 +40,8 @@ public class Map  {
 				
 				}
 				else{
+					//se entriamo nell'else vuol dire che abbiamo letto "/", cioè siamo arrivati a fine riga,
+					//la prossima stringa che leggeremo ci indicherà il tipo dei settori elencati nella nuova riga
 					tipoSettore=in.next();
 				}
 			}
@@ -45,29 +51,28 @@ public class Map  {
 		System.err.println("Errore nel leggere file Mappa");
 	}
 	
-	
 	}
 	
 	public void AddBorders(){
+	//questo metodo legge da file i confini dei settori e li aggiunge nella lista dei settori confinanti di ogni confine
+	// per fare questo leggerà per ogni riga il primo settore,che è il settore a cui aggiungere i confini, mentre
+	// i settori fino alla fine della riga sono i settori da aggiungere come confini
 		FileReader fileSettori;
 		try {
 			String nomeSettore;
 			String nomeConfine;
 			fileSettori = new FileReader("./src/main/java/it/polimi/ingsw/cg_5/Mappa_Galilei_Confini.txt");
 			Scanner in =new Scanner(fileSettori);
-			while(in.hasNextLine()){
-				while (in.hasNext() ){
+			while(in.hasNextLine()){				
 					nomeSettore=in.next();
 					nomeConfine=in.next();
 							while(nomeConfine.contains("/")==false){
+					//finchè non leggo"/" vuol dire che devo aggiungere la stringa che leggo come confine
+					//se esco dal ciclo vuol dire che ho letto "/", sono arrivato a fine riga, quindi cambierà
+					//il settore a cui aggiungere i confini
 								this.map.get(nomeSettore).addBorder(this.map.get(nomeConfine));
 								nomeConfine=in.next();
-								
-							}
-						
-										
-				}
-				
+							}				
 			}
 			in.close();
 			 
@@ -77,8 +82,10 @@ public class Map  {
 		
 	}
 	
-	public void printMap(){
-		Iterator<String> iterator = map.keySet().iterator();		  
+	public void printMap(){ 
+		//questo metodo stampa tutti gli elementi della mappa, sarà utile per implementare la CLI
+		Iterator<String> iterator = map.keySet().iterator();
+		//keySet restituisce tutte le chiavi degli oggetti contenuti nella Hashmap
 		while (iterator.hasNext()) {
 		   String key = iterator.next().toString();
 		   String value = map.get(key).toString();		  
