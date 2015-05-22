e1f48987b444d9029ee3789e41181988515fbf10
andrea.paulillo <andrea.paulillo@libero.it>
2015-05-22 17:47:49 +0200

    Prova Controller

---
+++ src/main/java/it/polimi/ingsw/cg_5/controller/Game.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.cg_5.controller;
+
+import it.polimi.ingsw.cg_5.model.*;
+
+
+public class Game {
+
+	private final int numberGame;
+	private GameState gameState;
+	
+	public Game(GameState gameState ,int numberGame){
+		this.numberGame=numberGame;
+		this.gameState=gameState;
+	}
+	
+	
+
+}
