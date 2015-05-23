889df2f1516f99658de74ea51e51427406b1dc55
Yasmin <Yasmin@192.168.0.118>
2015-05-23 13:44:05 +0200

    Deleted comment

--- src/main/java/it/polimi/ingsw/cg_5/model/Main.java
+++ src/main/java/it/polimi/ingsw/cg_5/model/Main.java
@@ -8,20 +8,7 @@ import java.util.Iterator;
 public class Main {
 
 	public static void main(String[] args) {
-		/*
-		Map prova= new Map();
-		prova.Generator();
-		prova.printMap();
-		prova.AddBorders();
-		prova.map.get("A03").bordersPrint();
-		HashSet <Sector> provalista = new HashSet <Sector> ();
-		provalista = prova.takeSector("A02").getReachableSectors(1);
-		System.out.println(provalista);
-		provalista = prova.takeSector("A02").getReachableSectors(2);
-		System.out.println(provalista);
-		provalista = prova.takeSector("A02").getReachableSectors(3);
-		System.out.println(provalista); 
-		*/
+
 	String players []=new String[5];
 		
 		for (int i=0 ; i<5; i++){
