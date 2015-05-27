7c5ee4e9b406e1729f2fd26990384d2e1173b0c8
andrea.paulillo <andrea.paulillo@libero.it>
2015-05-27 15:25:30 +0200

    Tolto Player

--- src/main/java/it/polimi/ingsw/cg_5/model/Alien.java
+++ src/main/java/it/polimi/ingsw/cg_5/model/Alien.java
@@ -3,10 +3,9 @@ package it.polimi.ingsw.cg_5.model;
 public class Alien extends Character {
 	
 
-	public Alien(String name){
-		super(name);
+	public Alien(String name,int playerID){
+		super(name,playerID);
 		maxMove = 2;
-		canAttack = true;
 	}
 
 	
---
+++ src/main/java/it/polimi/ingsw/cg_5/model/TurnState.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.cg_5.model;
+
+public enum TurnState {
+STARTED,HASMOVED,HASATTACKORDRAW,ENDTURN;
+}
